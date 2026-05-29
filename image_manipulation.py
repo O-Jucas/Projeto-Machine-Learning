c76d4d8dd10052f7b8397bdac0df0058e106847e
O-Jucas <lucasdecamposvargas@gmail.com>
2026-05-29 19:21:34 -0300

    Feat: Adicionando as principais funcoes e o terinametno
    Criamos funções com base no código que encontramos no kaggle. A ideia atual é usar três modelos e realizar a comparação.

---
+++ image_manipulation.py
@@ -0,0 +1,29 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+
+def load_images(image_paths: str, base_path: str, target_size=(64, 64)):
+    # Função que carrega cada imagem do caminho fornecido, redimensiona para 64x64 pixels e normaliza os valores dos pixels para o intervalo [0, 1].
+
+    images = []
+    for file_path in image_paths:
+        full_path = base_path / file_path
+        img = cv.imread(full_path, cv.IMREAD_GRAYSCALE)  # Load as grayscale
+        if img is not None:
+            img = cv.resize(img, target_size)  # Resize the image to 64x64 pixels
+            img = img / 255.0  # Normalize pixel values to [0, 1]
+        else:
+            img = np.zeros(target_size)  # If the image cannot be loaded, return a blank image
+        images.append(img)
+    return np.array(images)
+
+
+def show_image(image_number):
+    # Função que exibe a imagem correspondente ao número fornecido usando Matplotlib. A imagem é carregada usando a função load_images e exibida em escala de cinza.
+
+    image_array = load_images(df['image_path'].values)
+    plt.imshow(image_array[image_number], cmap='gray')
+    plt.axis('off') 
+    plt.show
---
+++ run.py
@@ -0,0 +1,58 @@
+import json
+import pandas as pd
+
+from sys import argv, exit
+from pathlib import Path
+
+from image_manipulation import load_images
+from training import encode_labels, create_cnn_model_1, create_cnn_model_2, create_cnn_model_3, training
+
+
+def main():
+
+    if len(argv) < 4:
+        print("Insira o caminho das imagens para o treino do modelo")
+        print(f"Forma de uso: python {argv[0]} <caminho-master-doodle-dataframe> <caminho-pasta-imagens> <json-words-to-keep>")
+        exit(-1)
+
+    # Verificando paths corretos
+
+    caminho_dataset = Path(argv[1])
+    assert caminho_dataset.exists(), "Caminho para o csv \"caminho-master-doodle-dataframe.csv\" incorreto"
+
+    caminho_pasta_imagens = Path(argv[2])
+    assert caminho_pasta_imagens.exists(), "Caminho para a pasta de imagens incorreto"
+
+    caminho_json = Path(argv[3])
+    assert caminho_json.exists(), "Caminho para o json de palavras selecionadas incorreto"
+
+    # Abrindo json palavras
+
+    with open(caminho_json, "r") as js:
+        words_to_keep = json.load(js)
+
+    assert isinstance(words_to_keep, list), "words_to_keep não é uma lista"
+    assert len(words_to_keep) > 0, "lista words_to_keep é vazio"
+
+    df = pd.read_csv(caminho_dataset)
+    df_filtrado = df[df['word'].isin(words_to_keep)]
+
+    image_array = load_images(df_filtrado['image_path'].values, base_path=caminho_pasta_imagens, target_size=(64, 64))
+
+    # Cria as colunas target numéricas com base no target string
+    encode_labels(df_filtrado['word'].values, words_to_keep)
+
+
+    # Cria modelo CNN
+    model1 = create_cnn_model_1(input_shape=(64, 64, 1), num_classes=len(words_to_keep))
+    model2 = create_cnn_model_2(input_shape=(64, 64, 1), num_classes=len(words_to_keep))
+    model3 = create_cnn_model_3(input_shape=(64, 64, 1), num_classes=len(words_to_keep))
+    
+
+    # Treina modelo e apresenta resultados
+    training(model1)
+    
+
+
+if __name__ == "__main__":
+    main()
